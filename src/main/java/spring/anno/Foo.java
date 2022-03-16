ccf83d0646f69743d5dc4c480b70e638f6f60e4e
Bingreee <shbyun9808@gmail.com>
2022-03-16 18:19:06 +0900

    commit day0316 pm

--- src/main/java/spring/anno/Foo.java
+++ src/main/java/spring/anno/Foo.java
@@ -6,7 +6,9 @@ import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Foo {
 	
 	@Autowired
